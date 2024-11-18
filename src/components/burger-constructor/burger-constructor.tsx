f8c572397d435bfa29b3e44013859980a68d24bd
Kseylo <alekseysarychev29@gmail.com>
2024-11-18 15:13:43 +0700

    feat: burger-constructor initial ui

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,3 +1,50 @@
+import {
+  Button,
+  ConstructorElement,
+} from '@ya.praktikum/react-developer-burger-ui-components'
+import { ingredients } from '@/utils/data.ts'
+import { Price } from '@/components/price'
+import styles from './burger-constructor.module.css'
+import { ConstructorItem } from '@/components/burger-constructor/constructor-item'
+
 export function BurgerConstructor() {
-  return <section></section>
+  const bun = ingredients[0]
+  const toppings = ingredients.filter(
+    (ingredient) => ingredient.type === 'main' || ingredient.type === 'sauce',
+  )
+
+  return (
+    <section className={'mt-25'}>
+      <ConstructorElement
+        type="top"
+        text={bun.name}
+        price={bun.price}
+        thumbnail={bun.image}
+        isLocked
+        extraClass={'ml-8 pl-4'}
+      />
+
+      <div className={`${styles.scroll} mt-4 pr-2`}>
+        {toppings.map((ingredient) => (
+          <ConstructorItem key={ingredient._id} ingredient={ingredient} />
+        ))}
+      </div>
+
+      <ConstructorElement
+        type="bottom"
+        text={bun.name}
+        price={bun.price}
+        thumbnail={bun.image}
+        isLocked
+        extraClass={'ml-8 pl-4 mt-4'}
+      />
+
+      <div className={`${styles.totalContainer} mt-10`}>
+        <Price price={610} size={'medium'} className={'mr-10'} />
+        <Button htmlType="button" type="primary" size="large">
+          Оформить заказ
+        </Button>
+      </div>
+    </section>
+  )
 }
