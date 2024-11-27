3f03027e3ff58dd4a2bcd014eb61424843858c54
Kseylo <alekseysarychev29@gmail.com>
2024-11-27 15:37:50 +0700

    chore: installed react-dnd

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import './index.css'
 import { App } from '@/components/app'
 import { store } from '@/store'
 import { Provider } from 'react-redux'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <DndProvider backend={HTML5Backend}>
+        <App />
+      </DndProvider>
     </Provider>
   </StrictMode>,
 )
