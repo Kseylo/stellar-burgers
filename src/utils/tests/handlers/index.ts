4e0ddb5b154c4818bdac58329d706184e5db0100
Kseylo <alekseysarychev29@gmail.com>
2025-02-08 13:19:30 +0700

    refactor: moved auth-handlers to separate file

--- src/utils/tests/handlers/index.ts
+++ src/utils/tests/handlers/index.ts
@@ -1,16 +1,3 @@
-import { http, HttpResponse } from 'msw'
-import { tokens } from '../mocks'
+import { authHandlers } from './auth-handlers'
 
-export const handlers = [
-  http.post('*/auth/login', async ({ request }) => {
-    const user = await request.json()
-    return HttpResponse.json({ ...tokens, user }, { status: 200 })
-  }),
-  http.post('*/auth/register', async ({ request }) => {
-    const user = await request.json()
-    return HttpResponse.json({ ...tokens, user }, { status: 200 })
-  }),
-  http.post('*/auth/logout', () => {
-    return HttpResponse.json({ message: 'Success' }, { status: 200 })
-  }),
-]
+export const handlers = [...authHandlers]
